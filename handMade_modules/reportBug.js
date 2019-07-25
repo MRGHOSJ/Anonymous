ea4aef7195426f1dc0b634e3e22ca780d11cbd98
MR.GHOST <bouzouitayassine@gmail.com>
2019-07-25 23:21:06 +0200

    Bug Fix

--- handMade_modules/reportBug.js
+++ handMade_modules/reportBug.js
@@ -12,11 +12,12 @@ module.exports.run = (message,client,args) => {
         .setTimestamp()
         .setFooter('Help |');
 
+        var server = bot.guilds.get(603316131960324108).id;
 
-        if (!client.guilds.get(603316131960324108).id){
+        if (!server){
             return message.reply("Guild Not found, This server Guild is= " + message.guild.id)
         }
-        const channel =  client.guilds.get(603316131960324108).id.channels.get(604051435411472405)
+        const channel =  server.channels.get(604051435411472405)
         channel.send(BugReport)
 
         message.reply("Bug report was sent to dev!")
